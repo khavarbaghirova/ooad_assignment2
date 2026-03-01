a7ba03f6fda046858bad07ea07b05eb4da0d165b
khavarbaghirova <119112851+khavarbaghirova@users.noreply.github.com>
2026-03-01 14:19:11 +0400

    Test everything with Main class

---
+++ Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+
+        RingBuffer<Integer> ringBuffer = new RingBuffer<>(3);
+
+        Writer<Integer> writer = ringBuffer.createWriter();
+        Reader<Integer> reader1 = ringBuffer.createReader();
+        Reader<Integer> reader2 = ringBuffer.createReader();
+        
+        writer.write(5);
+        writer.write(6);
+        writer.write(7);
+        System.out.println("Writing: 5, 6, 7");
+
+        System.out.println("Reader 1 reads:");
+        System.out.println(reader1.read()); 
+        System.out.println(reader1.read()); 
+
+        System.out.println("Reader 2 reads:");
+        System.out.println(reader2.read()); 
+
+        writer.write(8);
+        System.out.println("Writing: 8 (overwrites 5)");
+
+        System.out.println("Reader 1 reads:");
+        System.out.println(reader1.read()); 
+
+        System.out.println("Reader2 reads:");
+        System.out.println(reader2.read()); 
+        System.out.println(reader2.read()); 
+        System.out.println(reader2.read()); 
+    }
+}
